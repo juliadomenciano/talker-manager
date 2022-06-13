d8d52656612494f866167b1b204231204c11ff64
Maju Domenciano <juliadomenciano@hotmail.com>
2022-06-13 15:47:10 -0300

    exercicio 2 finalizado

--- index.js
+++ index.js
@@ -19,6 +19,16 @@ app.get('/talker', async (_req, res) => {
   res.status(200).json(content);
 });
 
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const file = await fs.readFile('./talker.json');
+  const content = JSON.parse(file);
+  const talker = content.find((i) => i.id === Number(id));
+  if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+
+  res.status(200).json(talker);
+});
+
 app.listen(PORT, () => {
   console.log('Online');
 });
